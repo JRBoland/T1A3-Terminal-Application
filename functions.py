341b09e996b682189e88a9e052f730430f4d0291
JRBoland <james.boland94@hotmail.com>
2022-12-09 21:56:35 +1100

    defined  main_menu functions and function calls based on menu options

--- functions.py
+++ functions.py
@@ -1,7 +1,46 @@
+#make sure you use pep8 styling!
+def main_menu():
+    print("\n\n_______________________________________\nxx/XX dogs have been fed today.\nXX total meals to be prepared.\nx meals to contain special dietary requirements.\n_______________________________________\n")
+    print("1. View dogs in shelter")
+    print("2. Add dog information")
+    print("3. Edit dog information")
+    print("4. Mark a dog as fed")
+    print("5. View dogs still to be fed")
+    print("6. Delete dog from database")
+    print("7. Exit")
+    while True:
+        try:
+            selection = int(input("\n\nEnter menu choice: "))
+            if selection == 1:
+                view_dogs()
+            elif selection == 2:
+                add_new_dog()
+            elif selection == 3:
+                edit_dog_info()
+            elif selection == 4:
+                mark_dog_as_fed()
+            elif selection == 5:
+                dogs_to_be_fed()
+            elif selection == 6:
+                remove_dog()
+            elif selection == 7:
+                exit()
+            else:
+                print("\n*** INVALID CHOICE ***\n*** Enter 1-7 ***")
+                main_menu()
+        except ValueError:
+            print("Invalid choice. Please enter 1-7.")
+
+def view_dogs():
+    print("*** View Dogs ***")
+    filler_pass = input("Enter anything to return to main menu: ")
+    main_menu()
+
 
 def add_new_dog():
     dogs = {}
     fed_dog_count = 0
+    print("\n*** Add dog information ***")
     while True:
         new_dog = {}
         name = input("\nWhat is the dogs name?: ")
@@ -84,3 +123,22 @@ def add_new_dog():
 #delete a doggy
 #new_dog_count -= 1
 
+def edit_dog_info():
+    print("*** Edit Dog info ***")
+    filler_pass = input("Enter anything to return to main menu: ")
+    main_menu()
+
+def mark_dog_as_fed():
+    print("*** Mark Dog as fed ***")
+    filler_pass = input("Enter anything to return to main menu: ")
+    main_menu()
+
+def dogs_to_be_fed():
+    print("*** Dogs to be fed ***")
+    filler_pass = input("Enter anything to return to main menu: ")
+    main_menu()
+
+def remove_dog():
+    print("*** Remove dog ***")
+    filler_pass = input("Enter anything to return to main menu: ")
+    main_menu()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import functions
 
-functions.add_new_dog()
-print("\n\nNew count of dogs is: ", functions.add_new_dog().new_dog_count)
-print("Number of dogs that have been fed today: " + str(functions.add_new_dog.fed_dog_count) + "/" + str(len(functions.add_new_dog.dogs)) + ".\n")
+functions.main_menu()
+#functions.add_new_dog()
+#print("\n\nNew count of dogs is: ", functions.add_new_dog().new_dog_count)
+#print("Number of dogs that have been fed today: " + str(functions.add_new_dog.fed_dog_count) + "/" + str(len(functions.add_new_dog.dogs)) + ".\n")

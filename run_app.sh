b7a9cc9e36b6c384905568031e87295f868e5b5d
JRBoland <james.boland94@hotmail.com>
2022-12-18 18:50:58 +1100

    updated run_app.sh bash script

--- run_app.sh
+++ run_app.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 if ! [[ -x "$(command -v python3)" ]]
 then
-  echo 'Error: 
-    This program runs on Python, but it looks like Python is not installed.
+  pyv="$(python -V 2>&1)"
+  if [[ $pyv == "Python 3"* ]]
+  then
+    python main.py
+  else
+    echo 'Error: 
+    This program runs on Python3, but it looks like Python3 is not installed.
     To install Python, check out https://installpython3.com/' >&2
+  fi
   exit 1
 else
-    echo 'Python is installed on this device. Beginning Dog Shelter Feeding Helper'
+    echo 'Preparing...'
 fi
-echo "Preparing..."
+echo "Installing requirements..."
 python3 -m venv .venv
 source venv/bin/activate
 pip install -r ./requirements.txt
